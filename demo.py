32cdd1548224fac53111a40377684ccc342980d6
aliabd <ali.si3luwa@gmail.com>
2021-07-22 12:28:09 -0700

    added auto flagging, and new auth

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import gradio as gr 
 
 
-def predict(inp, hospital):
+def predict(inp):
 	return "Looks good" 
 
 examples = [["logo.png"]]
@@ -9,6 +9,10 @@ examples = [["logo.png"]]
 css = """
 .input_image .edit_button {
 	visibility: hidden; # hides edit button
+}
+
+.flag {
+	visibility: hidden; # hides flag button 
 }
 
 .input_image {
@@ -20,19 +24,23 @@ css = """
 }
 """
 
-auth = [("user-1", "password-1"), ("user-2", "password-2")]
+
+# auth just checks if username field isn't empty, password can be empty
+def auth(username, password):
+	return len(username) > 0
+
+
 auth_message = "Welcome to the True Image interface! Please log in below."
 
-dropdown = gr.inputs.Dropdown(["Hostpial 1", "Hospital 2", "None"], default="None",	 label="Hospital")
 
-
-# allow_flagging and allow_screenshot set to false
+# allow_screenshot set to false
+# allow_flagging set to auto - every prediction will be saved at the source directory (where interface was launched)
 # modifying css via css parameter
 # set layout to vertical, but can remove this to keep it horizontal.
-iface = gr.Interface(predict, ["image", dropdown], "text", examples=examples, allow_flagging=False, allow_screenshot=False,
+iface = gr.Interface(predict, "image", "text", examples=examples, allow_flagging='auto', allow_screenshot=False,
 					 layout="vertical", css=css)
 
-# auth=auth for the two users
+# auth=auth
 # auth_message is optional, displays text on html log in page
 iface.launch(private_endpoint="https://trueimage.app/", auth=auth, auth_message=auth_message)
 
