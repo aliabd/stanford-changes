d758af0ff6b0801e9715791f16147c1bffc8b8d0
aliabd <ali.si3luwa@gmail.com>
2021-07-21 19:41:51 -0700

    added auth and dropdown

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import gradio as gr 
 
 
-def predict(inp):
+def predict(inp, hospital):
 	return "Looks good" 
 
 examples = [["logo.png"]]
@@ -20,11 +20,19 @@ css = """
 }
 """
 
+auth = [("user-1", "password-1"), ("user-2", "password-2")]
+auth_message = "Welcome to the True Image interface! Please log in below."
+
+dropdown = gr.inputs.Dropdown(["Hostpial 1", "Hospital 2", "None"], default="None",	 label="Hospital")
+
+
 # allow_flagging and allow_screenshot set to false
 # modifying css via css parameter
 # set layout to vertical, but can remove this to keep it horizontal.
-iface = gr.Interface(predict, "image", "text", examples=examples, allow_flagging=False, allow_screenshot=False,
+iface = gr.Interface(predict, ["image", dropdown], "text", examples=examples, allow_flagging=False, allow_screenshot=False,
 					 layout="vertical", css=css)
 
-iface.launch(private_endpoint="https://trueimage.app/") 
+# auth=auth for the two users
+# auth_message is optional, displays text on html log in page
+iface.launch(private_endpoint="https://trueimage.app/", auth=auth, auth_message=auth_message)
 
